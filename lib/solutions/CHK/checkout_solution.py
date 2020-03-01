fa4abf3f035907b6b61444309259502c83c62da3
sbx_user1051 <sbx_user1051@169.254.213.229>
2020-03-01 18:29:47 +0000

    Sun Mar 01 18:29:47 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,33 +1,3 @@
-# +------+-------+---------------------------------+
-# | Item | Price | Special offers                  |
-# +------+-------+---------------------------------+
-# | A    | 50    | 3A for 130, 5A for 200          |
-# | B    | 30    | 2B for 45                       |
-# | C    | 20    |                                 |
-# | D    | 15    |                                 |
-# | E    | 40    | 2E get one B free               |
-# | F    | 10    | 2F get one F free               |
-# | G    | 20    |                                 |
-# | H    | 10    | 5H for 45, 10H for 80           |
-# | I    | 35    |                                 |
-# | J    | 60    |                                 |
-# | K    | 70    | 2K for 120                      |
-# | L    | 90    |                                 |
-# | M    | 15    |                                 |
-# | N    | 40    | 3N get one M free               |
-# | O    | 10    |                                 |
-# | P    | 50    | 5P for 200                      |
-# | Q    | 30    | 3Q for 80                       |
-# | R    | 50    | 3R get one Q free               |
-# | S    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
-# | T    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
-# | U    | 40    | 3U get one U free               |
-# | V    | 50    | 2V for 90, 3V for 130           |
-# | W    | 20    |                                 |
-# | X    | 17    | buy any 3 of (S,T,X,Y,Z) for 45 |
-# | Y    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
-# | Z    | 21    | buy any 3 of (S,T,X,Y,Z) for 45 |
-# +------+-------+---------------------------------+
 item_price = {
     "A": 50,
     "B": 30,
@@ -39,7 +9,7 @@ item_price = {
     "H": 10,
     "I": 35,
     "J": 60,
-    "K": 80,
+    "K": 70,
     "L": 90,
     "M": 15,
     "N": 40,
@@ -47,14 +17,14 @@ item_price = {
     "P": 50,
     "Q": 30,
     "R": 50,
-    "S": 30,
+    "S": 20,
     "T": 20,
     "U": 40,
     "V": 50,
     "W": 20,
-    "X": 90,
-    "Y": 10,
-    "Z": 50,
+    "X": 17,
+    "Y": 20,
+    "Z": 21,
 }
 
 
@@ -143,37 +113,6 @@ def checkout(skus):
 
     sum = 0
 
-    # +------+-------+---------------------------------+
-    # | Item | Price | Special offers                  |
-    # +------+-------+---------------------------------+
-    # | A    | 50    | 3A for 130, 5A for 200          |
-    # | B    | 30    | 2B for 45                       |
-    # | C    | 20    |                                 |
-    # | D    | 15    |                                 |
-    # | E    | 40    | 2E get one B free               |
-    # | F    | 10    | 2F get one F free               |
-    # | G    | 20    |                                 |
-    # | H    | 10    | 5H for 45, 10H for 80           |
-    # | I    | 35    |                                 |
-    # | J    | 60    |                                 |
-    # | K    | 70    | 2K for 120                      |
-    # | L    | 90    |                                 |
-    # | M    | 15    |                                 |
-    # | N    | 40    | 3N get one M free               |
-    # | O    | 10    |                                 |
-    # | P    | 50    | 5P for 200                      |
-    # | Q    | 30    | 3Q for 80                       |
-    # | R    | 50    | 3R get one Q free               |
-    # | S    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
-    # | T    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
-    # | U    | 40    | 3U get one U free               |
-    # | V    | 50    | 2V for 90, 3V for 130           |
-    # | W    | 20    |                                 |
-    # | X    | 17    | buy any 3 of (S,T,X,Y,Z) for 45 |
-    # | Y    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
-    # | Z    | 21    | buy any 3 of (S,T,X,Y,Z) for 45 |
-    # +------+-------+---------------------------------+
-
     # apply Special offers
     skus_dict = get_another_free(skus_dict, "E", "B")
     skus_dict = get_another_free(skus_dict, "N", "M", 3)
@@ -203,12 +142,3 @@ def checkout(skus):
         sum += skus_dict[key] * item_price[key]
 
     return sum
-
-
-
-
-
-
-
-
-
