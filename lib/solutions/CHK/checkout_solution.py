27f734e95000884e2208d04c639927f086f4ac2c
sbx_user1051 <sbx_user1051@169.254.213.229>
2020-03-01 19:07:36 +0000

    Sun Mar 01 19:07:36 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -28,13 +28,13 @@ item_price = {
 }
 
 
-def group_discount(skus_dict, grouped_items, num, amount):
+def group_discount(num_items, grouped_items, num, amount):
     total = 0
 
     excess_group_items = {}
     for k in grouped_items:
-        if k in skus_dict and skus_dict[k] > 0:
-            excess_group_items[k] = skus_dict[k] % num
+        if k in num_items and num_items[k] > 0:
+            excess_group_items[k] = num_items[k]
 
     # order by most expensive excess_group_items
     ks = sorted(excess_group_items.keys(), key=item_price.get, reverse=True)
@@ -54,7 +54,7 @@ def group_discount(skus_dict, grouped_items, num, amount):
 
             # delete the carried items
             for key2 in carry_group_items:
-                skus_dict[key2] -= carry_group_items[key2]
+                num_items[key2] -= carry_group_items[key2]
 
             carry_group_items = {
                 key: (cnt + carry_cnt) - num
@@ -67,33 +67,33 @@ def group_discount(skus_dict, grouped_items, num, amount):
 
 
 # costs Y when you buy X of item
-def XforY(skus_dict, item, X, Y):
+def XforY(num_items, item, X, Y):
     total = 0
-    if item in skus_dict:
-        total = (skus_dict[item] // X) * Y
-        skus_dict[item] = skus_dict[item] % X
+    if item in num_items:
+        total = (num_items[item] // X) * Y
+        num_items[item] = num_items[item] % X
     return total
 
 
 # get other_item free when you buy num_for_free of item
-def get_another_free(skus_dict, item, other_item, num_for_free=2):
-    if other_item in skus_dict and item in skus_dict:
-        skus_dict[other_item] -= skus_dict[item] // num_for_free
+def get_another_free(num_items, item, other_item, num_for_free=2):
+    if other_item in num_items and item in num_items:
+        num_items[other_item] -= num_items[item] // num_for_free
 
-        if skus_dict[other_item] <= 0:
-            skus_dict[other_item] = 0
+        if num_items[other_item] <= 0:
+            num_items[other_item] = 0
 
 
 # buy num_for_free of item and get 1 free
-def get_one_free(skus_dict, item, num_for_free=2):
-    if item in skus_dict:
-        if skus_dict[item] <= num_for_free:
+def get_one_free(num_items, item, num_for_free=2):
+    if item in num_items:
+        if num_items[item] <= num_for_free:
             return
 
-        num_free = (skus_dict[item] // num_for_free)
-        if skus_dict[item] % num_for_free == 0:
+        num_free = (num_items[item] // num_for_free)
+        if num_items[item] % num_for_free == 0:
             num_free -= 1
-        skus_dict[item] -= num_free
+        num_items[item] -= num_free
 
 
 # noinspection PyUnusedLocal
@@ -102,48 +102,49 @@ def checkout(skus):
     skus = list(skus)
 
     # get number of items
-    skus_dict = {}
+    num_items = {}
     for sku in skus:
         if sku not in item_price.keys():
             return -1
 
-        if sku in skus_dict:
-            skus_dict[sku] += 1
+        if sku in num_items:
+            num_items[sku] += 1
         else:
-            skus_dict[sku] = 1
+            num_items[sku] = 1
 
     sum = 0
 
     # apply Special offers
-    get_another_free(skus_dict, "E", "B")
-    get_another_free(skus_dict, "N", "M", 3)
-    get_another_free(skus_dict, "R", "Q", 3)
-    get_one_free(skus_dict, "F")
-    get_one_free(skus_dict, "U", 3)
+    get_another_free(num_items, "E", "B")
+    get_another_free(num_items, "N", "M", 3)
+    get_another_free(num_items, "R", "Q", 3)
+    get_one_free(num_items, "F")
+    get_one_free(num_items, "U", 3)
 
-    sum += XforY(skus_dict, "A", 5, 200)
-    sum += XforY(skus_dict, "A", 3, 130)
-    sum += XforY(skus_dict, "B", 2, 45)
-    sum += XforY(skus_dict, "K", 2, 120)
-    sum += XforY(skus_dict, "H", 10, 80)
-    sum += XforY(skus_dict, "H", 5, 45)
-    sum += XforY(skus_dict, "P", 5, 200)
-    sum += XforY(skus_dict, "Q", 3, 80)
-    sum += XforY(skus_dict, "V", 3, 130)
-    sum += XforY(skus_dict, "V", 2, 90)
+    sum += XforY(num_items, "A", 5, 200)
+    sum += XforY(num_items, "A", 3, 130)
+    sum += XforY(num_items, "B", 2, 45)
+    sum += XforY(num_items, "K", 2, 120)
+    sum += XforY(num_items, "H", 10, 80)
+    sum += XforY(num_items, "H", 5, 45)
+    sum += XforY(num_items, "P", 5, 200)
+    sum += XforY(num_items, "Q", 3, 80)
+    sum += XforY(num_items, "V", 3, 130)
+    sum += XforY(num_items, "V", 2, 90)
 
-    sum += group_discount(skus_dict, ["S", "T", "X", "Y", "Z"], 3, 45)
-    sum += XforY(skus_dict, "S", 3, 45)
-    sum += XforY(skus_dict, "T", 3, 45)
-    sum += XforY(skus_dict, "X", 3, 45)
-    sum += XforY(skus_dict, "Y", 3, 45)
-    sum += XforY(skus_dict, "Z", 3, 45)
+    sum += group_discount(num_items, ["S", "T", "X", "Y", "Z"], 3, 45)
+    sum += XforY(num_items, "S", 3, 45)
+    sum += XforY(num_items, "T", 3, 45)
+    sum += XforY(num_items, "X", 3, 45)
+    sum += XforY(num_items, "Y", 3, 45)
+    sum += XforY(num_items, "Z", 3, 45)
 
     # handle rest of items
-    for key in skus_dict:
-        sum += skus_dict[key] * item_price[key]
+    for key in num_items:
+        sum += num_items[key] * item_price[key]
 
     return sum
+
 
 
 
