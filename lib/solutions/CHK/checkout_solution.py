fad2aad34af07e674f81a05792dc1a2574504964
sbx_user1051 <sbx_user1051@169.254.213.229>
2020-03-01 16:02:14 +0000

    Sun Mar 01 16:02:14 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,18 @@
 
+# +------+-------+----------------+
+# | Item | Price | Special offers |
+# +------+-------+----------------+
+# | A    | 50    | 3A for 130     |
+# | B    | 30    | 2B for 45      |
+# | C    | 20    |                |
+# | D    | 15    |                |
+# +------+-------+----------------+
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    print(skus)
+    sum = 0
+    skus = sorted(skus)
+    
+    for sku in skus:
 
--- test/solution_tests/CHK_R1/test_check.py
+++ test/solution_tests/CHK_R1/test_check.py
@@ -4,4 +4,3 @@
 # class TestSum():
 #     def test_sum(self):
 #         assert checkout_solution.checkout(1, 2) == 3
-
