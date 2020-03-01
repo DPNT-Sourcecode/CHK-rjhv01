d8dcfad747d47bb38109b15cda0e4789cb540b7d
sbx_user1051 <sbx_user1051@169.254.213.229>
2020-03-01 17:36:02 +0000

    Sun Mar 01 17:36:02 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -59,36 +59,7 @@ def get_one_free(skus_dict, item, num_for_free=2):
     return skus_dict
 
 
-# +------+-------+------------------------+
-# | Item | Price | Special offers         |
-# +------+-------+------------------------+
-# | A    | 50    | 3A for 130, 5A for 200 |
-# | B    | 30    | 2B for 45              |
-# | C    | 20    |                        |
-# | D    | 15    |                        |
-# | E    | 40    | 2E get one B free      |
-# | F    | 10    | 2F get one F free      |
-# | G    | 20    |                        |
-# | H    | 10    | 5H for 45, 10H for 80  |
-# | I    | 35    |                        |
-# | J    | 60    |                        |
-# | K    | 80    | 2K for 150             |
-# | L    | 90    |                        |
-# | M    | 15    |                        |
-# | N    | 40    | 3N get one M free      |
-# | O    | 10    |                        |
-# | P    | 50    | 5P for 200             |
-# | Q    | 30    | 3Q for 80              |
-# | R    | 50    | 3R get one Q free      |
-# | S    | 30    |                        |
-# | T    | 20    |                        |
-# | U    | 40    | 3U get one U free      |
-# | V    | 50    | 2V for 90, 3V for 130  |
-# | W    | 20    |                        |
-# | X    | 90    |                        |
-# | Y    | 10    |                        |
-# | Z    | 50    |                        |
-# +------+-------+------------------------+
+
 
 item_price = {
     "A": 50,
@@ -137,17 +108,55 @@ def checkout(skus):
 
     sum = 0
 
+    # +------+-------+------------------------+
+    # | Item | Price | Special offers         |
+    # +------+-------+------------------------+
+    # | A    | 50    | 3A for 130, 5A for 200 |
+    # | B    | 30    | 2B for 45              |
+    # | C    | 20    |                        |
+    # | D    | 15    |                        |
+    # | E    | 40    | 2E get one B free      |
+    # | F    | 10    | 2F get one F free      |
+    # | G    | 20    |                        |
+    # | H    | 10    | 5H for 45, 10H for 80  |
+    # | I    | 35    |                        |
+    # | J    | 60    |                        |
+    # | K    | 80    | 2K for 150             |
+    # | L    | 90    |                        |
+    # | M    | 15    |                        |
+    # | N    | 40    | 3N get one M free      |
+    # | O    | 10    |                        |
+    # | P    | 50    | 5P for 200             |
+    # | Q    | 30    | 3Q for 80              |
+    # | R    | 50    | 3R get one Q free      |
+    # | S    | 30    |                        |
+    # | T    | 20    |                        |
+    # | U    | 40    | 3U get one U free      |
+    # | V    | 50    | 2V for 90, 3V for 130  |
+    # | W    | 20    |                        |
+    # | X    | 90    |                        |
+    # | Y    | 10    |                        |
+    # | Z    | 50    |                        |
+    # +------+-------+------------------------+
+
     skus_dict = get_another_free(skus_dict, "E", "B")
+    skus_dict = get_another_free(skus_dict, "N", "M", 3)
+    skus_dict = get_another_free(skus_dict, "R", "Q", 3)
+    skus_dict = get_one_free(skus_dict, "F")
     sum += XforY(skus_dict, "A", 5, 200)
     sum += XforY(skus_dict, "A", 3, 130)
     sum += XforY(skus_dict, "B", 2, 45)
-    skus_dict = get_one_free(skus_dict, "F")
+    sum += XforY(skus_dict, "H", 10, 80)
+    sum += XforY(skus_dict, "H", 5, 45)
+    sum += XforY(skus_dict, "P", 5, 200)
+    sum += XforY(skus_dict, "Q", 3, 80)
 
     # handle rest of items
     for key in skus_dict:
         sum += skus_dict[key] * item_price[key]
 
     return sum
+
 
 
 
