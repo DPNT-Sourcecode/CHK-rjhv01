a2a2631e0c4401ab9806cb3b13a2bc4562c1efa2
sbx_user1051 <sbx_user1051@169.254.213.229>
2020-03-01 16:11:14 +0000

    Sun Mar 01 16:11:14 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,7 +13,7 @@ ALLOWED_ITEMS = ["A", "B", "C", "D"]
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    skus = sorted(skus)
+    skus = list(skus)
     skus_dict = {}
     for sku in skus:
         if sku not in ALLOWED_ITEMS:
@@ -22,5 +22,12 @@ def checkout(skus):
             skus_dict[sku] += 1
         else:
             skus_dict = 1
+
+    sum = 0
+
+    # handle special offers
+    if "A" in skus_dict:
+        sum += skus_dict["A"] // 3
+
 
 
